edfa698519f4f5fadc6a3bc74926cb9609eaf8a9
Liam Bowen <liambowen@gmail.com>
2015-09-12 04:36:48 +0000

    unicode, format-sql, cool ctags

---
+++ home/.emacs.d/elpa/list-unicode-display-20150219.101/list-unicode-display-autoloads.el
@@ -0,0 +1,24 @@
+;;; list-unicode-display-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "list-unicode-display" "list-unicode-display.el"
+;;;;;;  (21999 14479 134885 47000))
+;;; Generated autoloads from list-unicode-display.el
+
+(autoload 'list-unicode-display "list-unicode-display" "\
+Display a list of unicode characters with names matching REGEXP.
+If no regexp is supplied, all characters are shown.  This takes
+some time.
+
+\(fn &optional REGEXP)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; list-unicode-display-autoloads.el ends here
---
+++ home/.emacs.d/elpa/list-unicode-display-20150219.101/list-unicode-display-pkg.el
@@ -0,0 +1,1 @@
+(define-package "list-unicode-display" "20150219.101" "Search for and list unicode characters by name" '((cl-lib "0.5")) :keywords '("convenience"))
