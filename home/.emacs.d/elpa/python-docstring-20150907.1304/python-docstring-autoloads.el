6e83837752e2ff3b5eaf2a0db27825e42b81ce2e
Liam Bowen <LiamBowen@gmail.com>
2015-10-22 20:13:00 -0400

    Update a bunch of packages

---
+++ home/.emacs.d/elpa/python-docstring-20150907.1304/python-docstring-autoloads.el
@@ -0,0 +1,16 @@
+;;; python-docstring-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("python-docstring-pkg.el" "python-docstring.el")
+;;;;;;  (22055 54050 148207 549000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; python-docstring-autoloads.el ends here
